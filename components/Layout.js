f00f593b55032caadd5b80582971f448715c84c7
ken liu <kliu@mbira.com>
2022-12-01 23:14:30 -0600

    Update layout.js. Add header, footer styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import React, { Children } from 'react';
 
 export default function Layout({ title, children }) {
@@ -10,9 +11,25 @@ export default function Layout({ title, children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
-        <header>header</header>
-        <main>{children}</main>
-        <footer>footer</footer>
+        <header>
+          <nav className="flex h-12 px-4 items-center justify-between shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              Online Buddha
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex shadow-inner h-10 justify-center items-center">
+          Copywrite &copy; 2021-{new Date().getFullYear()} Online Buddha
+        </footer>
       </div>
     </>
   );
