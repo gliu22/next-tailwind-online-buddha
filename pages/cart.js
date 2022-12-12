8691a3e03e26104d94e1575c1f7d9a8c627a8073
ken liu <kliu@mbira.com>
2022-12-11 22:11:00 -0600

    push cart function

---
+++ pages/cart.js
@@ -0,0 +1,106 @@
+import React, { useContext } from 'react';
+import Layout from '../components/Layout';
+import { Store } from '../utils/Store';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Router } from 'next/router';
+import { useRouter } from 'next/router';
+
+export default function CartScreen() {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const removeItemHandler = (item) => {
+    dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  };
+
+  return (
+    <Layout title="Shopping Cart">
+      <h1 className="mb-4 text-xl">Shopping Cart</h1>
+      {cartItems.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <table className="min-w-full">
+              <thead className="border-b">
+                <tr>
+                  <th className="px-5 text-lfet">Item</th>
+                  <th className="p-5 text-right">Quantity</th>
+                  <th className="p-5 text-right">Price</th>
+                  <th className="p-5">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.slug} className="border-b">
+                    <td>
+                      <Link
+                        className="flex item-center"
+                        href={`/product/${item.slug}`}
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        ></Image>
+                        &nbsp;
+                        {item.name}
+                      </Link>
+                    </td>
+                    <td className="p-5 text-right">{item.quantity}</td>
+                    <td className="p-5 text-right">${item.price}</td>
+                    <td className="p-5 text-center">
+                      <button onClick={() => removeItemHandler(item)}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="card p-5">
+            <ul>
+              <li>
+                <div className="pb-3 text-xl">
+                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                  items) : $
+                  {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                </div>
+              </li>
+              <li>
+                <button
+                  className="primary-button w-full"
+                  onClick={() => router.push('/shipping')}
+                  type="button"
+                >
+                  Check Out
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </Layout>
+  );
+}
